163700c494195d88356d7bbffd2f4f84b7b223eb
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-10-14 18:33:31 -0400

    pf fix 8

--- 2-programming101/pf_small_8.py
+++ 2-programming101/pf_small_8.py
@@ -1,9 +1,10 @@
 try:
     start = int(input("Choose any whole number to start with.\n"))
     end = int(input("Choose any whole number to end with.\n"))
-    while start <  end:
-        start += 1
+    print("Thank you!")
+    while start <= end:
         print(start)
+        start += 1
 except ValueError:
     print("I said, choose a whole number!")
 
